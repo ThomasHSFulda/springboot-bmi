a9182281bd367484b63ab9c0decab5cd21f218db
Thomas <thomas-maximilian.niestroj@informatik.hs-fulda.de>
2023-11-16 13:26:13 +0100

    working example after azure cloud test

--- src/main/java/com/example/bmi/BmiApplication.java
+++ src/main/java/com/example/bmi/BmiApplication.java
@@ -2,11 +2,13 @@ package com.example.bmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 /**
  * Author: Thomas Niestroj
  * Created: 07.11.2023
  * */
 @SpringBootApplication
+@ComponentScan({"DogApi"})
 public class BmiApplication {
 
 	public static void main(String[] args) {
